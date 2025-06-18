0c0ec7af1a3c3b100cb9716b8ddc9be4019321e9
AmbrosiaAI <kybe.511@gmail.com>
2025-06-18 19:19:56 +1000

    Добавлена табличка с рекламой Hyper Light Drifter и крутые костры. бновлён скайбокс

--- Assets/Scripts/AppleManager.cs
+++ Assets/Scripts/AppleManager.cs
@@ -6,27 +6,27 @@ using UnityEngine;
 public class AppleManager : MonoBehaviour
 {
 
-    [SerializeField] private GameObject applePrefab;
-    private GameObject _apple;
+    [SerializeField] private GameObject objectPrefab;
+    private GameObject _object;
     public float max_X = 1;
     public float min_X = 0;
     public float max_Z = 1;
     public float min_Z = 0;
     void Update()
     {
-        if (_apple == null)
+        if (_object == null)
         {
             Vector3 spawnPosition = new Vector3(UnityEngine.Random.Range(min_X, max_X), this.transform.position.y, UnityEngine.Random.Range(min_Z, max_Z));
             //Vector3 spawnPosition = new Vector3 (0,0,0);
 
             if (!Physics.CheckSphere(spawnPosition, 3f, 1))
             {
-                _apple = Instantiate(applePrefab) as GameObject;
-                _apple.transform.position = spawnPosition;
+                _object = Instantiate(objectPrefab) as GameObject;
+                _object.transform.position = spawnPosition;
                 float angle = UnityEngine.Random.Range(0, 360);
-                _apple.transform.Rotate(0, angle, 0);
+                _object.transform.Rotate(0, angle, 0);
             }
-            else { Debug.Log("Неправильный спавн яблока"); }
+            else { Debug.Log($"Неправильный спавн объекта {objectPrefab}"); }
         }
     }
 }
